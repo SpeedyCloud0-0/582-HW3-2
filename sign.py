1df130c5f3226226c68ba85780c41527ad74a837
SpeedyCloud0-0 <63207198+SpeedyCloud0-0@users.noreply.github.com>
2021-05-31 08:18:42 -0700

    verification deleted

--- sign.py
+++ sign.py
@@ -12,7 +12,7 @@ def sign(m):
 
 	#generate signature
 	r, s = ecdsa.sign(m, private_key, hashfunc=sha256)
-	valid = ecdsa.verify((r, s), m, public_key, hashfunc=sha256)
+	# valid = ecdsa.verify((r, s), m, public_key, hashfunc=sha256)
 
 	assert isinstance( public_key, point.Point )
 	assert isinstance( r, int )
