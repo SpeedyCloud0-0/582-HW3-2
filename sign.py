dbf54474dac52bd395414cbca52dcbe7cd1c11c9
SpeedyCloud0-0 <63207198+SpeedyCloud0-0@users.noreply.github.com>
2021-05-30 23:23:40 -0700

    verification added

--- sign.py
+++ sign.py
@@ -12,7 +12,8 @@ def sign(m):
 
 	#generate signature
 	#Your code here
-	r, s = ecdsa.sign(m, private_key)
+	r, s = ecdsa.sign(m, private_key, hashfunc=sha256)
+	valid = ecdsa.verify((r, s), m, public_key, hashfunc=sha256)
 
 	assert isinstance( public_key, point.Point )
 	assert isinstance( r, int )
