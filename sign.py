74475bfe48872d61a20dc561701129e235620604
SpeedyCloud0-0 <63207198+SpeedyCloud0-0@users.noreply.github.com>
2021-05-30 23:36:09 -0700

    add hashfunc=

--- sign.py
+++ sign.py
@@ -11,12 +11,13 @@ def sign(m):
 	private_key, public_key = keys.gen_keypair(curve.secp256k1)
 
 	#generate signature
-	r, s = ecdsa.sign(m, private_key, sha256)
-	# valid = ecdsa.verify((r, s), m, public_key)
+	r, s = ecdsa.sign(m, private_key, hashfunc=sha256)
 
 	assert isinstance( public_key, point.Point )
 	assert isinstance( r, int )
 	assert isinstance( s, int )
+
 	return( public_key, [r,s] )
+
 
 
